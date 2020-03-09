d34271844980236be6c9d57fd5b1eb1c64773987
HCS Learning <learning@hardcybersoft.cl>
2020-03-09 00:41:08 -0300

    script funcionando

--- wordpress-install-centos8.sh
+++ wordpress-install-centos8.sh
@@ -1,17 +1,3 @@
-# variables
-CANT_INSTALL=3
-WP_URL="https://es.wordpress.org/latest-es_ES.tar.gz"
-WP_TAR="latest-es_ES.tar.gz"
-WP_CLI_URL="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
-WP_INSTALL_BASE=/var/www/wp
-WP_USER_PREFIX=`cat ~/.wpuser`
-WP_PASSWD=`cat ~/.wppass`
-MAIN_DOMAIN=`cat ~/.wpmaindomain`
-MYSQL_USER=root
-MYSQL_PASSWD=`cat ~/.mysqlpass`
-MYSQL_PASSWD_CONFIG=~/.my.cnf
-
-
 # instalar software necesario
 sudo yum install firewalld
 sudo yum install nginx
@@ -34,6 +20,28 @@ sudo systemctl enable php-fpm
 sudo systemctl enable mariadb
 
 
+# URLS software
+WP_TAR="latest-es_ES.tar.gz"
+WP_URL="https://es.wordpress.org/$WP_TAR"
+WP_CLI_URL="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+
+# ir al HOME
+cd
+# descargar wordpress
+wget $WP_URL
+# Instalo wp-cli para poder instalar de manera headless
+curl -O $WP_CLI_URL
+
+# variables
+CANT_INSTALL=20
+WP_INSTALL_BASE=/var/www/wp
+WP_USER_PREFIX=`cat ~/.wpuser`
+WP_PASSWD=`cat ~/.wppass`
+MAIN_DOMAIN=`cat ~/.wpmaindomain`
+MYSQL_USER=root
+MYSQL_PASSWD=`cat ~/.mysqlpass`
+MYSQL_PASSWD_CONFIG=~/.my.cnf
+
 # configuración para conectar sin tener que escribir la password de MySQL
 cat /dev/null > $MYSQL_PASSWD_CONFIG
 echo "[mysql]" >> $MYSQL_PASSWD_CONFIG
@@ -41,13 +49,6 @@ echo "user=$MYSQL_USER" >> $MYSQL_PASSWD_CONFIG
 echo "password=$MYSQL_PASSWD" >> $MYSQL_PASSWD_CONFIG
 chmod 0600 $MYSQL_PASSWD_CONFIG
 
-# descargar wordpress
-cd
-wget $WP_URL
-
-
-# Instalo wp-cli para poder instalar de manera headless
-curl -O $WP_CLI_URL
 
 # crear sitios
 for i in $(seq 1 $CANT_INSTALL)
@@ -65,16 +66,21 @@ do
     echo "drop database if exists $BD_NAME; create database $BD_NAME;" | mysql -u root
     
     # instalacion headless con wp-cli
-    wp core config --dbhost=localhost --dbname=$BD_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWD --extra-php="define('FS_METHOD', true);"
+    cd $dir/wordpress
+    wp core config --dbhost=localhost --dbname=$BD_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWD --extra-php="define('FS_METHOD', 'direct');"
     wp core install --url=$domain --title="Sitio $i" --admin_name="$WP_USER_PREFIX$i" --admin_password="$WP_PASSWD" --admin_email="wp-s$i@$MAIN_DOMAIN"
 
     # sobrescribo configuración para descarga directar de plugins y themes
     #ex -s -c '20i|define("FS_METHOD", "direct");' -c x wp-config.php
 
     # permisos post instalación    
-    cd $dir/wordpress/wp-content 
-    sudo chown vultr:apache -R plugins themes uploads languages upgrade
-    sudo chmod g+rwx -R plugins themes uploads languages upgrade
+    wpcontent=$dir/wordpress/wp-content 
+    
+    cd $wpcontent
+    sudo chown vultr:apache $wpcontent
+    sudo chmod 775 $wpcontent
+    sudo chown vultr:apache -R plugins themes uploads languages
+    sudo chmod 775 -R plugins themes uploads languages
 done
 
 
@@ -92,6 +98,8 @@ sudo firewall-cmd --zone=public --list-services
 # configurar selinux
 sudo sestatus 
 sudo setsebool -P httpd_can_network_connect on
+sudo setsebool -P httpd_can_network_connect_db on
+sudo setsebool -P mysql_connect_http on
 sudo semanage fcontext -l
 sudo semanage fcontext -a -t httpd_sys_rw_content_t "/var/www(/.*)*/uploads(/.*)?"
 sudo semanage fcontext -a -t httpd_sys_rw_content_t "/var/www(/.*)*/wp-content(/.*)?"
